9a9313460b99a6e72d1770237a3e894b8dcb9587
bhushankelkar <kelkarkulbhushan@gmail.com>
2024-04-20 21:34:12 +0530

    Filtering records

--- src/main/java/in/bhushan/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/in/bhushan/expensetrackerapi/controller/ExpenseController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
 
@@ -27,6 +28,20 @@ public class ExpenseController {
         return expenseService.getAllExpenses();
     }
 
+    @GetMapping("/expenses/name")
+    public List<Expense> getAllExpensesByName(@RequestParam String keyword,Pageable page){
+        return expenseService.readByName(keyword,page);
+    }
+
+    @GetMapping("/expenses/date")
+    public List<Expense> getAllExpensesBetweenDate(@RequestParam(required = false) Date startDate,@RequestParam(required = false) Date endDate, Pageable page){
+        return expenseService.readByDate(startDate,endDate,page);
+    }
+
+    @GetMapping("/expenses/category")
+    public List<Expense> getAllExpensesByCategory(@RequestParam String category,Pageable page){
+        return expenseService.readByCategory(category,page);
+    }
 //    @GetMapping("/expenses")
 //    public List<Expense> getAllExpenses(Pageable page){
 //        return expenseService.getAllExpenses(page).toList();
